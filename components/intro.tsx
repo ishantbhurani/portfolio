c8e78801cf63b5ca6cdbfd393765fb09342bb0cf
Ishant Bhurani <67356291+ishantbhurani@users.noreply.github.com>
2023-09-21 23:06:35 +0530

    change p to h1

--- components/intro.tsx
+++ components/intro.tsx
@@ -41,7 +41,7 @@ export default function Intro() {
         </div>
       </div>
 
-      <motion.p
+      <motion.h1
         className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
@@ -50,7 +50,7 @@ export default function Intro() {
         <span className='font-bold'>full-stack developer</span>. I enjoy
         building <span className='italic'>sites and apps</span>, and solving
         complex problems. My focus is <span className='underline'>MERN</span>.
-      </motion.p>
+      </motion.h1>
 
       <motion.div
         className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium'
