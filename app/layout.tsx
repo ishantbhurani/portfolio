a738e6c1b41c89c59467589595ba30206ba80bd1
Ishant Bhurani <67356291+ishantbhurani@users.noreply.github.com>
2023-09-21 20:48:37 +0530

    add intro section with avatar img

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={`${inter.className} bg-gray-50 text-gray-950 relative`}>
+      <body
+        className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36`}
+      >
         <div className='bg-[#fbe2e3] absolute h-[31.25rem] w-[31.25rem] -z-10 rounded-full -top-24 right-44 blur-[10rem] sm:w-[68.75rem]'></div>
         <div className='bg-[#dbd7fb] absolute h-[31.25rem] w-[50rem] -z-10 rounded-full -top-4 -left-[35rem] right-44 blur-[10rem] sm:w-[68.75rem] md:-left-[33rem] lg:-left-[28rem] xl:-left-60 2xl:-left-20'></div>
         <Header />
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Intro from '@/components/intro'
+
 export default function Home() {
-  return <main>Home</main>
+  return (
+    <main>
+      <Intro />
+    </main>
+  )
 }
