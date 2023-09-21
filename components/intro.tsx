000331a046b31c938adf6d0979c3830a4d2c5718
Ishant Bhurani <67356291+ishantbhurani@users.noreply.github.com>
2023-09-21 22:09:41 +0530

    add content and actions to intro section

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,12 +1,15 @@
 'use client'
 
+import Link from 'next/link'
 import Image from 'next/image'
 import { motion } from 'framer-motion'
+import { BsArrowRight, BsGithub, BsLinkedin, BsTwitter } from 'react-icons/bs'
+import { HiDownload } from 'react-icons/hi'
 import avatarImg from '@/public/avatarImg.jpg'
 
 export default function Intro() {
   return (
-    <section>
+    <section className='mb-28 max-w-[50rem] text-center sm:mb-0'>
       <div className='flex items-center justify-center'>
         <div className='relative'>
           <motion.div
@@ -37,6 +40,65 @@ export default function Intro() {
           </motion.span>
         </div>
       </div>
+
+      <motion.p
+        className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className='font-bold'>Hello, I'm Ishant Bhurani.</span> I'm a{' '}
+        <span className='font-bold'>full-stack developer</span>. I enjoy
+        building <span className='italic'>sites and apps</span>, and solving
+        complex problems. My focus is <span className='underline'>MERN</span>.
+      </motion.p>
+
+      <motion.div
+        className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium'
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.1 }}
+      >
+        <Link
+          href='#contact'
+          className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full whitespace-nowrap outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition'
+        >
+          Contact me here{' '}
+          <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition' />
+        </Link>
+
+        <a
+          href='/CV.pdf'
+          download
+          className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full whitespace-nowrap outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10'
+        >
+          Download CV{' '}
+          <HiDownload className='opacity-60 group-hover:translate-y-1 transition' />
+        </a>
+
+        <a
+          href='https://www.linkedin.com/in/ishant-bhurani'
+          target='_blank'
+          className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition border border-black/10'
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          href='https://github.com/ishantbhurani'
+          target='_blank'
+          className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full text-[1.35rem] outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition border border-black/10'
+        >
+          <BsGithub />
+        </a>
+
+        <a
+          href='https://twitter.com/IShaunt'
+          target='_blank'
+          className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full text-[1.35rem] outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition border border-black/10'
+        >
+          <BsTwitter />
+        </a>
+      </motion.div>
     </section>
   )
 }
