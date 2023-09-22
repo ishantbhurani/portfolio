81c0d32f36ef35bc4b58a932cc521e1db173eab6
Ishant Bhurani <67356291+ishantbhurani@users.noreply.github.com>
2023-09-22 14:10:13 +0530

    add experiences section

---
+++ components/experience.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import React from 'react'
+import SectionHeading from './section-heading'
+import { Chrono } from 'react-chrono'
+import { experiencesData } from '@/lib/data'
+
+export default function Experience() {
+  return (
+    <section id='experience' className='mb-28 scroll-mt-28 sm:mb-40'>
+      <SectionHeading>My experience</SectionHeading>
+      <Chrono
+        items={experiencesData
+          .map(item => ({
+            title: item.date,
+            cardTitle: item.title,
+            cardSubtitle: item.location,
+            cardDetailedText: item.description,
+          }))
+          .reverse()}
+        mode='VERTICAL_ALTERNATING'
+        theme={{
+          primary: '#e5e7eb',
+          secondary: 'white',
+          cardBgColor: '#f3f4f6',
+          titleColor: 'black',
+          titleColorActive: 'black',
+          cardTitleColor: 'black',
+        }}
+      >
+        <div className='chrono-icons'>
+          {experiencesData.map((item, index) => (
+            <div key={index} className='text-2xl bg-white'>
+              {item.icon}
+            </div>
+          ))}
+        </div>
+      </Chrono>
+    </section>
+  )
+}
