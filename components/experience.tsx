40a0c02d92f0698815b7724b6d2700a18f5ddade
Ishant Bhurani <67356291+ishantbhurani@users.noreply.github.com>
2023-09-22 14:14:59 +0530

    add section ref

--- components/experience.tsx
+++ components/experience.tsx
@@ -4,10 +4,13 @@ import React from 'react'
 import SectionHeading from './section-heading'
 import { Chrono } from 'react-chrono'
 import { experiencesData } from '@/lib/data'
+import { useSectionInView } from '@/lib/hooks'
 
 export default function Experience() {
+  const { ref } = useSectionInView('Experience', 0.5)
+
   return (
-    <section id='experience' className='mb-28 scroll-mt-28 sm:mb-40'>
+    <section id='experience' ref={ref} className='mb-28 scroll-mt-28 sm:mb-40'>
       <SectionHeading>My experience</SectionHeading>
       <Chrono
         items={experiencesData
