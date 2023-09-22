e34c0ce9e3e8a98a5552638675e7c56b88d40627
Ishant Bhurani <67356291+ishantbhurani@users.noreply.github.com>
2023-09-22 23:38:43 +0530

    add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import ActiveSectionContextProvider from '@/context/active-section-context'
 import { Toaster } from 'react-hot-toast'
+import Footer from '@/components/footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -28,6 +29,7 @@ export default function RootLayout({
         <ActiveSectionContextProvider>
           <Header />
           {children}
+          <Footer />
 
           <Toaster position='top-right' />
         </ActiveSectionContextProvider>
---
+++ components/footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer>
+      <small>&copy; 2023 Ishant Bhurani. All rights reserved.</small>
+      <p>
+        <span>About this website:</span> built with React & Next.js (App Router
+        & Server Actions), TypeScript, Tailwind CSS, Framer Motion, React Email
+        & Resend, Vercel hosting.
+      </p>
+    </footer>
+  )
+}
